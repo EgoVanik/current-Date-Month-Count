04ffe1bdc53efb40e102a7c2f63c2844ee0a8f6a
Vanik Egoyan <47858761+EgoVanik@users.noreply.github.com>
2021-09-09 21:58:30 +0300

    Update and rename current-Date-Month-Count.js to counter.js

--- counter.js
+++ counter.js
@@ -1,19 +1,18 @@
-// Небольшой счетчик, который автоматически отображает текущую дату, текущий месяц (в кириллице) и какое кол-во товара осталось
-// разрабатывался для посадочной страницы на flexbe.
+// Небольшой счетчик, который отображает текущую дату, текущий месяц (в кириллице) и остаток товара
 document.addEventListener('DOMContentLoaded', () => {
-    const myCounterSelector = document.querySelector('#myCounter'),// селектор кол-ва штук
+    const counterSelector = document.querySelector('#myCounter'),// селектор кол-ва штук
           currentDateSelector = document.querySelector('#currentDate'),// селектор текущего дня
           currentMonthSelector = document.querySelector('#currentMonth'),// селектор месяца
-          currentDate = new Date().getDate(),// из newDate() получаем Date, на выходе дата
-          currentMonth = new Date().getMonth(),// получаем index месяца
-          currentDay = new Date().getDay();// получаем index дня недели
+          currentDate = new Date().getDate(),// получаем дату
+          currentMonth = new Date().getMonth(),// получаем индекс месяца
+          currentDay = new Date().getDay();// получаем индекс дня недели
 
     currentDateSelector.textContent = currentDate;// в селектор даты, задаем текстовое значение элемента равное текущей дате
 
     // с помощью конструкции switch сравниваем каждый индекс месяца с текущим и устанавливаем название месяца в кириллице
     switch (currentMonth) {
         case 0:
-            currentMonthSelector.textContent = ' января';// задаем текстовым значением название месяца в селектор месяца
+            currentMonthSelector.textContent = ' января';
             break;
 
         case 1:
@@ -66,32 +65,31 @@ document.addEventListener('DOMContentLoaded', () => {
     // с помошью switch сравниваем index дня с текущим днем и определяем какое кол-во штук осталось
     switch (currentDay) {
         case 0:
-            myCounterSelector.textContent = 7;// задаем текстовое значение в элемент
+            counterSelector.textContent = 7;// задаем текстовое значение в элемент
         break;
 
         case 1:
-            myCounterSelector.textContent = 5;
+            counterSelector.textContent = 5;
         break;
 
         case 2:
-            myCounterSelector.textContent = 11;
+            counterSelector.textContent = 11;
         break;
 
         case 3:
-            myCounterSelector.textContent = 9;
+            counterSelector.textContent = 9;
         break;
 
         case 4:
-            myCounterSelector.textContent = 7;
+            counterSelector.textContent = 7;
         break;
 
         case 5:
-            myCounterSelector.textContent = 11;
+            counterSelector.textContent = 11;
         break;
 
         case 6:
-            myCounterSelector.textContent = 9;
+            counterSelector.textContent = 9;
         break;
     }    
-
 });
